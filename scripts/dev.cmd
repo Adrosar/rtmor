e4cedcf77b878b25933361f6562c3618209840c0
Adrian Gargula <>
2022-05-26 22:52:28 +0200

    Fix for DEV

--- scripts/dev.cmd
+++ scripts/dev.cmd
@@ -4,6 +4,7 @@ set "SCRIPTS_DIR=%~dp0"
 set "PROJECT_DIR=%SCRIPTS_DIR%.."
 set "BUILD_DIR=%PROJECT_DIR%\build"
 cd "%PROJECT_DIR%\cmd\dev"
-go build -o "%BUILD_DIR%" && "%BUILD_DIR%\dev.exe" %*
+go build -o "%BUILD_DIR%"
 cd "%PROJECT_DIR%"
+"%BUILD_DIR%\dev.exe" %*
 endlocal
